a8d1e63c1ea572e970266148d5705df079b90e68
qazi <qazi.naveed@nextgeni.com>
2025-01-09 09:53:01 +0100

    provide localized message via AppError

--- Source/Core/Networking/Error/AppError.swift
+++ Source/Core/Networking/Error/AppError.swift
@@ -6,12 +6,73 @@
 //
 
 public enum AppError: Error, Equatable {
+    
+    case decodingError(error: Error)
+    case networkError(error: Error)
+    case invalidBaseUrl(description: String)
+    case invalidUrl(description: String)
+    case missingCriticalField(String)
+    case apiError(message: String)
+    case httpError(statusCode: Int)
+    case emptyData
+    case noWeatherDataAvailable
+    case invalidWeatherData
+    case invalidAPIKey
+    case invalidCityName
+    case invalidTime
+    case invalidResponse
+    case unknownError
+
+}
+
+extension AppError {
+    
+    public static func getLocalizedErrorMessage(error: Error) -> String {
+        var errorMessage: String = ""
+        
+        switch error {
+        case let appError as AppError:
+            switch appError {
+            case .invalidResponse:
+                errorMessage = "The response received is invalid."
+            case .unknownError:
+                errorMessage =  "An unknown error occurred."
+            case .decodingError(let error):
+                errorMessage =  "Decoding error: \(error.localizedDescription)"
+            case .invalidBaseUrl(let description), .invalidUrl(let description):
+                errorMessage =  "Invalid URL: \(description)"
+            case .httpError(let statusCode):
+                errorMessage =  "HTTP Error with status code \(statusCode)."
+            case .emptyData:
+                errorMessage =  "Received empty data from the server."
+            case .noWeatherDataAvailable:
+                errorMessage =  "No weather data available."
+            case .invalidWeatherData:
+                errorMessage =  "The weather data is invalid."
+            case .missingCriticalField(let field):
+                errorMessage =  "Missing critical field: \(field)"
+            case .invalidAPIKey:
+                errorMessage =  "Invalid API key."
+            case .invalidCityName:
+                errorMessage =  "Invalid city name."
+            case .networkError(let error):
+                errorMessage =  "Network error: \(error.localizedDescription)"
+            case .apiError(let message):
+                errorMessage =  "API Error: \(message)"
+            case .invalidTime:
+                errorMessage =  "Invalid Time."
+            }
+        default:
+            errorMessage = "An unexpected error occurred."
+        }
+        return errorMessage
+    }
+    
     public static func == (lhs: AppError, rhs: AppError) -> Bool {
             switch (lhs, rhs) {
             case (.invalidResponse, .invalidResponse),
                  (.unknownError, .unknownError),
                  (.emptyData, .emptyData),
-                 (.githubTokenNotfound, .githubTokenNotfound),
                  (.noWeatherDataAvailable, .noWeatherDataAvailable),
                  (.invalidWeatherData, .invalidWeatherData),
                  (.invalidAPIKey, .invalidAPIKey),
@@ -32,20 +93,4 @@ public enum AppError: Error, Equatable {
                 return false
             }
         }
-    case invalidResponse
-    case unknownError
-    case decodingError(error: Error)
-    case invalidBaseUrl(description: String)
-    case invalidUrl(description: String)
-    case httpError(statusCode: Int)
-    case emptyData
-    case githubTokenNotfound
-    case noWeatherDataAvailable
-    case invalidWeatherData
-    case missingCriticalField(String)
-    case invalidAPIKey
-    case invalidCityName
-    case networkError(error: Error)
-    case apiError(message: String)
-    case invalidTime
 }
--- Source/Core/Shared/Views/ReusableViews/DefaultErrorView.swift
+++ Source/Core/Shared/Views/ReusableViews/DefaultErrorView.swift
@@ -17,47 +17,11 @@ struct DefaultErrorView: View {
             Spacer()
         }
         .background(AppColors.primaryBackground.color)
-
+        
     }
-        private var errorMessage: String {
-            switch error {
-            case let appError as AppError:
-                switch appError { 
-                case .invalidResponse:
-                    return "The response received is invalid."
-                case .unknownError:
-                    return "An unknown error occurred."
-                case .decodingError(let error):
-                    return "Decoding error: \(error.localizedDescription)"
-                case .invalidBaseUrl(let description), .invalidUrl(let description):
-                    return "Invalid URL: \(description)"
-                case .httpError(let statusCode):
-                    return "HTTP Error with status code \(statusCode)."
-                case .emptyData:
-                    return "Received empty data from the server."
-                case .githubTokenNotfound:
-                    return "GitHub token not found."
-                case .noWeatherDataAvailable:
-                    return "No weather data available."
-                case .invalidWeatherData:
-                    return "The weather data is invalid."
-                case .missingCriticalField(let field):
-                    return "Missing critical field: \(field)"
-                case .invalidAPIKey:
-                    return "Invalid API key."
-                case .invalidCityName:
-                    return "Invalid city name."
-                case .networkError(let error):
-                    return "Network error: \(error.localizedDescription)"
-                case .apiError(let message):
-                    return "API Error: \(message)"
-                case .invalidTime:
-                    return "Invalid Time."
-                }
-            default:
-                return error.localizedDescription
-            }
-        }
+    private var errorMessage: String {
+        AppError.getLocalizedErrorMessage(error: error)
+    }
 }
 
 #Preview {
